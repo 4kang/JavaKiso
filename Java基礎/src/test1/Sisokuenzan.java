d20389b3db48479e83b57a991ae9c25edca269f7
kono mayu <konotori08@gmail.com>
2023-01-08 17:57:41 +0900

    Third commit

---
+++ Java基礎/src/test1/Sisokuenzan.java
@@ -0,0 +1,20 @@
+package test1;
+
+public class Sisokuenzan {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//鍾離の財布
+		int saifu = 1000;
+		//商品の値段
+		int item = 1000;
+		
+		System.out.println("鍾離は商品を購入しようする。");
+		System.out.println("店員「その商品は5割引だよ」");
+		
+		System.out.println(saifu - item / 2 + "円になります。");
+
+	}
+
+}
