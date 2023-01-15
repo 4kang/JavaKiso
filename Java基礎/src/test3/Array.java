c8980dfa22d543fba3957f49894d21f64433f922
kono mayu <konotori08@gmail.com>
2023-01-15 21:02:38 +0900

    Add

---
+++ Java基礎/src/test3/Array.java
@@ -0,0 +1,20 @@
+package test3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>();
+		
+		list.add("炎の剣");
+		list.add("普通の防具");
+		list.add("回復薬");
+		
+		System.out.println(list);
+
+	}
+
+}
