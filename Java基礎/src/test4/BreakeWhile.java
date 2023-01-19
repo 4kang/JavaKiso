4a598accdb1d1c6bd9639bd2046dac362299020f
kono mayu <konotori08@gmail.com>
2023-01-19 21:17:21 +0900

    Add break

--- Java基礎/src/test4/BreakeWhile.java
+++ Java基礎/src/test4/BreakeWhile.java
@@ -4,6 +4,7 @@ public class BreakeWhile {
 
 	public static void main(String[] args) {
 		
+		//for
 		int num = 0;
 		
 		for(int i = 0; i <= 100; i++) {
@@ -14,8 +15,37 @@ public class BreakeWhile {
 				num += i;
 			}
 		}
-		
 		System.out.println("偶数の合計：" + num);
+		
+		//While
+		int num1 = 0;
+		int i = 0;
+		
+		while(i <= 100) {
+			if(i==50) {
+				break;
+			}
+			if(i%2==0) {
+				num1 += i;
+			}
+			i++;
+		}
+		System.out.println("偶数の合計：" + num1);
+		
+		//do-while
+		int num2 = 0;
+		int x = 0;
+		
+		do {
+			if(x==50) {
+				break;
+			}
+			if(x%2==0) {
+				num2 += x;
+			}
+			x++;
+		}while(x <= 100);
+		System.out.println("偶数の合計：" + num2);
 	}
 
 }
