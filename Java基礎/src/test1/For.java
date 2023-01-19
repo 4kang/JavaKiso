1c8c123d4de2e370f38ee8f57e49f40fa2047ce6
kono mayu <konotori08@gmail.com>
2023-01-19 17:58:40 +0900

    Add for while

--- Java基礎/src/test1/For.java
+++ Java基礎/src/test1/For.java
@@ -3,7 +3,7 @@ package test1;
 public class For {
 	public static void main(String[] args) {
 		
-		for (int i = 0; i <= 3; i++) {
+		/*for (int i = 0; i <= 3; i++) {
 			System.out.println("i=" + i);
 		}
 		
@@ -14,7 +14,21 @@ public class For {
 			sum += i;
 		}
 		
-		System.out.println("合計＝" + sum);
+		System.out.println("合計＝" + sum);*/
+		
+		int sum1 = 0;
+		int sum2 = 0;
+		
+		for(int i = 1; i <= 100; i++) {
+			if(i%2==0) {
+				sum1 += i;
+			}else {
+				sum2 += i;
+			}
+		}
+		
+		System.out.println("偶数の合計=" + sum1);
+		System.out.println("奇数の合計=" + sum2);
 	}
 
 }
--- Java基礎/src/test4/Forwhile.java
+++ Java基礎/src/test4/Forwhile.java
@@ -4,10 +4,37 @@ public class Forwhile {
 
 	public static void main(String[] args) {
 		
-		for (int x = 0; x < 100; x++) {
-			
+		int sum1 = 0; //偶数
+		int sum2 = 0; //奇数
+		
+		for (int x = 0; x <= 100; x++) {
+			if(x%2==0) {
+				sum1 += x;
+			}else {
+				sum2 += x;
+			}
 		}
+		
+		System.out.println("-----for文の四則演算-----");
+		System.out.println("偶数の合計" + sum1);
+		System.out.println("奇数の合計" + sum2);
+		
+		int i = 0;
+		int sum3 = 0;
+		int sum4 = 0;
+		
+		while(i <= 100) {
+			if(i%2==0) {
+				sum3 += i;
+			}else {
+				sum4 += i;
+			}
+			i++;
+		}
+		
+		System.out.println("-----whileの四則演算-----");
+		System.out.println("偶数の合計" + sum3);
+		System.out.println("奇数の合計" + sum4);
 
 	}
-
 }
