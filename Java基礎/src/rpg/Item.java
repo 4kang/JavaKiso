33aeb0735be779600b878b723af3b32525c92a2d
kono mayu <konotori08@gmail.com>
2023-01-23 22:00:23 +0900

    Add item

--- Java基礎/src/rpg/Item.java
+++ Java基礎/src/rpg/Item.java
@@ -2,5 +2,21 @@
 package rpg;
 
 public class Item {
+	private String name; //アイテム名
+	private int count;//アイテム数
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count=count;
+	}
 
 }
