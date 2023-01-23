682d0436bc4d4e8d79ad8908bb45dc1d787715cf
kono mayu <konotori08@gmail.com>
2023-01-23 18:08:26 +0900

    Add

--- Java基礎/src/rpg/Rpg.java
+++ Java基礎/src/rpg/Rpg.java
@@ -4,7 +4,14 @@ package rpg;
 public class Rpg {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		Hero shujinko = new Hero();
+		
+		//set○○で値をセットする
+		shujinko.setName("勇者A");
+		shujinko.setAttac(20);
+		shujinko.setHp(100);
+		
+		System.out.println("彼の名前は" + shujinko.getName());
 
 	}
 
