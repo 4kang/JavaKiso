0a5edb65170a00e15d5fdff492d3eaec9d0df8cf
kono mayu <konotori08@gmail.com>
2023-01-24 22:03:02 +0900

    Add

--- Java基礎/src/rpg/Rpg.java
+++ Java基礎/src/rpg/Rpg.java
@@ -22,8 +22,13 @@ public class Rpg {
 		Maou satan = new Maou();
 		satan.setAttac(10);
 		satan.setHp(200);
-
 		
+		System.out.println("-------------------------");
+		System.out.println(hero.getName() + "の行動を決めろ！");
+		System.out.println();
+		Scanner scanner = new Scanner(System.in);
+		 
+		/*魔王の体力もしくは主人公の体力が無くなるまで戦闘の繰り返しを行う*/
 	}
 
 }
