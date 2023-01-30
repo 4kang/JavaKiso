eea0726e962779ceeb2431e9b030ae74b3cbc716
kono mayu <konotori08@gmail.com>
2023-01-30 18:52:30 +0900

    Add

--- Java基礎/src/progete/Person.java
+++ Java基礎/src/progete/Person.java
@@ -1,11 +1,27 @@
 package progete;
 
 public class Person {
+	//インスタンスフィールド
 	public String firstName;
 	public String lastName;
+	public int age;
+	public double height;
+	public double weight;
+	
+	Person(String firstName, String lastName, int age, double height, double weight){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
 	
 	public String fullName() {
 		return this.firstName + " " + lastName;
+	}
+	
+	public double bmi() {
+		return this.weight/height/height;
 	}
 
 }
