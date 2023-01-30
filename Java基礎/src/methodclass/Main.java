337a34b9a27a49adf1f9b5e8823862a626ffa932
kono mayu <konotori08@gmail.com>
2023-01-30 15:46:47 +0900

    Add

--- Java基礎/src/methodclass/Main.java
+++ Java基礎/src/methodclass/Main.java
@@ -1,5 +1,6 @@
 package methodclass;
 
+import java.util.Scanner;
 import java.lang.Math;
 
 public class Main { //Mainクラス
@@ -10,9 +11,28 @@ public class Main { //Mainクラス
 		Person.printData(Person.fullName("Kate", "Jones"), 27, 1.6, 50.0);
 		Person.printData(Person.fullName("John","Christopher", "Smith"), 65, 1.75, 80.0);
 		
-		System.out.println("******************************");
 		int max = Math.max(3, 8);
 		System.out.println("最大値は" + max + "です");
+		System.out.println("******************************");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("名前：");
+		String firstName = scanner.next();
+		
+		System.out.print("名字：");
+		String lastName = scanner.next();
+		
+		System.out.print("年齢：");
+		int age = scanner.nextInt();
+		
+		System.out.print("身長(m)：");
+		double height = scanner.nextDouble();
+		
+		System.out.print("体重(㎏)：");
+		double weight = scanner.nextDouble();
+		
+		Person.printData(Person.fullName(firstName, lastName),age,height,weight);
 	}
 	
 } //classMain
