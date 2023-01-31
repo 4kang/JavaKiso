df0a7cd6eef5b5572d3744d304129b60f144d2e6
kono mayu <konotori08@gmail.com>
2023-01-31 19:06:47 +0900

    Add middleName

--- Java基礎/src/progete/Main.java
+++ Java基礎/src/progete/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		Person person1 = new Person("ディルック","ラグウィンド",27,1.8,68.0);
 		person1.printData();
 		
-		Person person2 = new Person("ガイア","アルベルヒ",26,1.8,64.0);
+		Person person2 = new Person("ガイア","G","アルベルヒ",26,1.8,64.0);
 		person2.printData();
 		
 		Person.printCount();
