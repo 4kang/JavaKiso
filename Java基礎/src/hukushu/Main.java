55a1007a874a1dfbc7cce0ac4999d0a4dc5c6463
kono mayu <konotori08@gmail.com>
2023-04-09 21:02:43 +0900

    Add hukushu

---
+++ Java基礎/src/hukushu/Main.java
@@ -0,0 +1,13 @@
+package hukushu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Hukushu hukushu1 = new Hukushu("クロノア","メメントモリ",18,1.7,60.0);
+		
+		
+	}
+
+}
